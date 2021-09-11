83f20d41aa11cc8a8f3dda3bb4dee3ec4330fe1b
UserJHansen <47265397+UserJHansen@users.noreply.github.com>
2021-09-11 21:30:03 +1000

    Fix Testing

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
+import { SWRConfig } from 'swr';
+
+import mangaAPIFetcher from '../APIs/mangaAPI';
 import Router from '../router';
 
 describe('App', () => {
   it('should render', () => {
-    expect(render(<Router />)).toBeTruthy();
+    expect(
+      render(
+        <Suspense fallback={<>Loading... </>}>
+          <SWRConfig
+            value={{
+              fetcher: mangaAPIFetcher,
+              revalidateOnFocus: false,
+              revalidateOnReconnect: false,
+            }}
+          >
+            <Router />
+          </SWRConfig>
+        </Suspense>
+      )
+    ).toBeTruthy();
   });
 });
