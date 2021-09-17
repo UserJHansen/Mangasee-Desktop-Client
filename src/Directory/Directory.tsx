8432c0e560bd65ce2945314dc3a108ed25332cca
UserJHansen <47265397+UserJHansen@users.noreply.github.com>
2021-09-18 00:35:55 +1000

    Starting the Directory Screen

--- src/Directory/Directory.tsx
+++ src/Directory/Directory.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import useSWR from 'swr';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { Link } from 'react-router-dom';
 
-import { Col } from 'react-bootstrap';
+import { decode } from 'he';
+import { Alert, Col } from 'react-bootstrap';
 import { faFolderOpen } from '@fortawesome/free-regular-svg-icons';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import Store from '../APIs/storage';
 import Box from '../Global/Box';
 
 export default function Directory() {
   const { data: FullDirectory } = useSWR('/api/FullDirectory');
+  const [Loaded, setLoaded] = React.useState([]);
+  const [firstLoad, setfirstLoad] = React.useState(true);
+  const loadMore = () =>
+    setLoaded(FullDirectory.Directory.slice(0, Loaded.length + 20));
 
   const store = new Store();
   if (!FullDirectory) return <>Loading...</>;
   if (navigator.onLine) {
     store.set('lastFullDirectory', FullDirectory);
+  }
+  if (firstLoad) {
+    loadMore();
+    setfirstLoad(false);
   }
   return (
     <>
@@ -24,6 +36,28 @@ export default function Directory() {
         title={`Directory (${FullDirectory.Directory.length})`}
       >
         A full list of all Manga available on MangaSee
+        <InfiniteScroll
+          dataLength={Loaded.length}
+          next={loadMore}
+          scrollThreshold={0.5}
+          hasMore={Loaded.length < FullDirectory.Directory.length}
+          loader={
+            <Alert variant="primary">
+              <FontAwesomeIcon icon={faSpinner} spin /> Loading Directory...
+            </Alert>
+          }
+        >
+          {Loaded.map((manga: { i: string; st: string; s: string }) => (
+            <div key={manga.s} className="top-15">
+              <Link to={`/manga/${manga.i}`}>
+                {decode(manga.s)}
+                <i style={{ color: '#CCC' }}>
+                  {manga.st === 'Complete' ? ' Complete' : ''}
+                </i>
+              </Link>
+            </div>
+          ))}
+        </InfiniteScroll>
       </Box>
       <Col lg={4}>
         <Link to="/manga/Berserk">
